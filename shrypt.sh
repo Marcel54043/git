52865d57e7ef46a2ed604252a2f3b84399a4a0bd
Marcel <dsw54043@student.dsw.edu.pl>
2024-05-04 18:14:47 +0200

    komentarz

--- shrypt.sh
+++ shrypt.sh
@@ -4,6 +4,12 @@ dzisiaj=$(date +"%Y-%m-%d")
 
 if [[ "$1" == "--date" ]]; then
     echo "Dzisiaj jest: $dzisiaj"
+elif [[ "$1" == "--logs" ]]; then
+    for ((i=1; i<=100; i++)); do
+        echo "Nazwa pliku: log$i.txt" > "log$i.txt"
+        echo "Utworzony przez: $0" >> "log$i.txt"
+        echo "Data utworzenia: $dzisiaj" >> "log$i.txt"
+    done
 else
-    echo "Spróbuj: $0 --date"
+    echo "Użycie: $0 [--date | --logs]"
 fi
