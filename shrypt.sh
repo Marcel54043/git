eadd4391a0b6fc26d3d15850b4ab3b4c1c7b1449
Marcel <dsw54043@student.dsw.edu.pl>
2024-05-04 18:53:33 +0200

    komentarz3

--- shrypt.sh
+++ shrypt.sh
@@ -24,9 +24,19 @@ elif [[ "$1" == "--help" || "$1" == "-h" ]]; then
     echo "Opcje:"
     echo "  --date  -d    Wyświetla dzisiejszą datę."
     echo "  --logs <n>  -l   Tworzy n plików dziennika z datą i nazwą skryptu."
+elif [[ "$1" == "--init" ]]; then
+    git clone <https://github.com/Marcel54043/git.git>
+
+    repo_path=$(pwd)
+
+    echo "export PATH=\"\$PATH:$repo_path\"" >> ~/.bashrc
+    source ~/.bashrc
+
+    echo "Repozytorium zostało pomyślnie zainicjowane."
 else
-    echo "Użycie: $0 [--date | --logs]"
+    echo "Użycie: $0 [--date | --logs | --init]"
     echo "Opcje:"
     echo "  --date   -d    Wyświetla dzisiejszą datę."
     echo "  --logs <n>  -l  Tworzy n plików dziennika z datą i nazwą skryptu."
+    echo "  --init   Inicjuje środowisko pracy, klonując repozytorium do bieżącego katalogu i ustawiając ścieżkę w zmiennej PATH."
 fi
