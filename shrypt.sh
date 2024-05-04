716291ee4ae37907768718dbc493fc3a6969de43
Marcel <dsw54043@student.dsw.edu.pl>
2024-05-04 18:19:36 +0200

    komentarz

--- shrypt.sh
+++ shrypt.sh
@@ -5,11 +5,20 @@ dzisiaj=$(date +"%Y-%m-%d")
 if [[ "$1" == "--date" ]]; then
     echo "Dzisiaj jest: $dzisiaj"
 elif [[ "$1" == "--logs" ]]; then
-    for ((i=1; i<=100; i++)); do
+    if [[ $# -ne 2 || ! $2 =~ ^[0-9]+$ ]]; then
+        echo "Użycie: $0 --logs <liczba_plików>"
+        exit 1
+    fi
+
+    liczba_plikow=$2
+
+    for ((i=1; i<=$liczba_plikow; i++)); do
         echo "Nazwa pliku: log$i.txt" > "log$i.txt"
         echo "Utworzony przez: $0" >> "log$i.txt"
         echo "Data utworzenia: $dzisiaj" >> "log$i.txt"
     done
+
+    echo "$liczba_plikow plików zostało utworzonych."
 else
     echo "Użycie: $0 [--date | --logs]"
 fi
